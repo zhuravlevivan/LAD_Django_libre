1a349f697cbc3c25ecb980a2a9d2f3467f2e2501
legion <855434@gmail.com>
2023-11-27 22:31:32 +0300

    add migrations, model, db, requirements

--- libre/libre_authors/models.py
+++ libre/libre_authors/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class LibreAuthors(models.Model):
+	title = models.CharField(max_length=255)
+	author = models.CharField(max_length=255)
+	country = models.CharField(max_length=255)
+	content = models.TextField(blank=True)
+	time_create = models.DateTimeField(auto_now_add=True)
+	time_update = models.DateTimeField(auto_now=True)
