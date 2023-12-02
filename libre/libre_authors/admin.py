963bf134f27f75833acf8cb953e5506bf0e60f6c
legion <855434@gmail.com>
2023-12-02 21:18:29 +0300

    add pagination in admin panel, auto-slug field based on title

--- libre/libre_authors/admin.py
+++ libre/libre_authors/admin.py
@@ -9,9 +9,10 @@ from .models import LibreAuthors
 class LibreAuthorsAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'author', 'time_create', 'is_published']
     list_display_links = ['id', 'title']
-    ordering = ['time_create', 'title']
     list_filter = ['id', 'title', 'author', 'time_create']
     list_editable = ('is_published', )
-    list_per_page = 4
+    list_per_page = 6
+    prepopulated_fields = {'slug': ('title',), }
+    ordering = ['time_create', 'title']
 
     search_fields = ['title', 'author', 'content']
