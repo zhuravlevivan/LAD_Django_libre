a3bb65ccd7c909d884d85efedc6b516107c187bc
legion <855434@gmail.com>
2023-12-03 21:07:06 +0300

    add base login view

--- libre/users/urls.py
+++ libre/users/urls.py
@@ -1,10 +1,16 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', views.login_user, name='login'),  # 'users:login'
+    # path('login/', views.login_user, name='login'),  # 'users:login'
+    path('login/', LoginView.as_view(
+        template_name='users/login.html',
+        redirect_authenticated_user=True,
+    ),
+         name='login'),
     path('logout/', views.logout_user, name='logout'),  # 'users:logout'
 
 ]
