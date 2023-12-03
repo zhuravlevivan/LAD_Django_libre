041eb2c1309c830947057f0ece7a43bce5b353b9
legion <855434@gmail.com>
2023-12-03 16:37:37 +0300

    add login/logout

---
+++ libre/libre_authors/utils.py
@@ -0,0 +1,4 @@
+menu = [
+    {'title': 'Books', 'url_name': 'books'},
+    {'title': 'About', 'url_name': 'about'},
+]
---
+++ libre/users/context_processors.py
@@ -0,0 +1,5 @@
+from libre_authors.utils import menu
+
+
+def get_libre_authors_context(request):
+    return {'mainmenu': menu}
